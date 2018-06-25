be4e57c12770ca1d5ce5e16be69d47a0ecfac640
Devin Cunningham <dtyler.cunningham@gmail.com>
2018-06-25 15:54:28 -0700

    saving

--- src/router/car-router.js
+++ src/router/car-router.js
@@ -37,6 +37,68 @@ carRouter.post('/api/cars', jsonParser, (req, res) => {
         return res.sendStatus(409);  
       }
       logger.log(logger.ERROR, `Car Router GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
+      return res.sendStatus(500);
     });
   return undefined;
 });
+
+carRouter.get('/api/cars/:id?', (req, res) => {
+  logger.log(logger.INFO, 'Car Router GET api/cars/:id = processing req');
+  return Car.findOne({ _id: req.params.id })
+    .then((car) => {
+      if (!car) {
+        logger.log(logger.INFO, 'Car Router GER api/cars/:id responding with 404 status code for no car');
+        return res.sendStatus(404);
+      }
+      logger.log(logger.INFO, 'Car Router GET api/cars/:id responding with 200 status code for successful GET');
+      return res.json(car);
+    })
+    .catch((err) => {
+      if (err.message.toLowerCase().includes('cast to objected failed')) {
+        logger.log(logger.ERROR, `Car Router PUT: responding with 404 status code to mongdb error, objectId ${req.params.id} failed, ${err.message}`);
+        return res.sendStatus(404);  
+      }
+      logger.log(logger.ERROR, `Car Router GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
+      return res.sendStatus(500);
+    });
+});
+
+carRouter.put('/api/cars/:id?', jsonParser, (req, res) => {
+  if (!req.params.id) {
+    logger.log(logger.INFO, 'Car Router PUT api/cars: Responding with 400 code for no id passed in');
+    return res.sendStatus(400);
+  }
+
+  const options = {
+    new: true,
+    runValidators: true,
+  };
+
+  Car.init()
+    .then(() => {
+      return Car.findByIdAndUpdate(req.params.id, req.body, options);
+    })
+    .then((updatedCar) => {
+      logger.log(logger.INFO, `Car Router PUT responding with a 200 status code for successful update to car: ${updatedCar}`);
+      return res.json(updatedCar);
+    })
+    .catch((err) => {
+      if (err.message.toLowerCase().includes('cast to objected failed')) {
+        logger.log(logger.ERROR, `Car Router PUT: responding with 404 status code to mongdb error, objectId ${req.params.id} failed, ${err.message}`);
+        return res.sendStatus(404);  
+      }
+      if (err.message.toLowerCase().includes('validation failed')) {
+        logger.log(logger.ERROR, `Car Router PUT: responding with 400 status code bad request  ${err.message}`);
+        return res.sendStatus(400);  
+      }
+      if (err.message.toLowerCase().includes('duplicate key')) {
+        logger.log(logger.ERROR, `Car Router PUT: responding with 409 status code duplicate key  ${err.message}`);
+        return res.sendStatus(409);  
+      }
+      logger.log(logger.ERROR, `Car Router GET: 500 status code for unaccounted error ${JSON.stringify(err)}`);
+      return res.sendStatus(500);
+    });
+  return undefined;
+});
+
+export default carRouter;
